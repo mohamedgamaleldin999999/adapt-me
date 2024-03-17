2462c89c204d565c934e992e4c515c8729b67362
mohamedgamaleldin999999 <123296170+mohamedgamaleldin999999@users.noreply.github.com>
2024-03-17 03:28:52 +0200

    Use prettier

--- src/App.js
+++ src/App.js
@@ -1,32 +1,23 @@
-const pet = (props) => {
-  return React.createElement('div', {}, [
-    React.createElement('h1', {}, props.name),
-    React.createElement('h2', {}, props.animal),
-    React.createElement('h2', {}, props.breed)
-  ])
-}
+const pet = (props) => {   return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("h2", {}, props.animal),
+    React.createElement("h2", {}, props.breed),
+  ]);
+};
 
 const App = () => {
-  return React.createElement(
-    'div',
-    {},
-    [
-      React.createElement(
-        'h1',
-        {},
-        'adopt me'
-      ),
-      React.createElement(pet, {
-        animal: 'Dog',
-        name: 'sheetos',
-        breed: 'havens'
-      }),
-      React.createElement(pet),
-      React.createElement(pet),
-    ]
-  )
-}
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "adopt me"),
+    React.createElement(pet, {
+      animal: "Dog",
+      name: "sheetos",
+      breed: "havens",
+    }),
+    React.createElement(pet),
+    React.createElement(pet),
+  ]);
+};
 
-const container = document.getElementById('root');
-const root = ReactDOM.createRoot(container)
-root.render(React.createElement(App, {}, 'sdsdsdsdsdsds'))
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+root.render(React.createElement(App, {}, "sdsdsdsdsdsds"));
